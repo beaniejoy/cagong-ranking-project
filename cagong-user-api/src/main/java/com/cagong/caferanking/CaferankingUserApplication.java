e5ebb68e3ade52cae24baaf7e4f2999ed5310179
hanbinleejoy <lhb1025@gmail.com>
2020-05-28 21:11:34 +0900

    200528 [All] yml 설정 변경

--- cagong-user-api/src/main/java/com/cagong/caferanking/CaferankingUserApplication.java
+++ cagong-user-api/src/main/java/com/cagong/caferanking/CaferankingUserApplication.java
@@ -9,8 +9,7 @@ public class CaferankingUserApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "classpath:dev.yml,"
-			+ "classpath:aws.yml";
+			+ "classpath:dev.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(CaferankingUserApplication.class)
