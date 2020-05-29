a2b775f107e90577e1c7be564da8748b540921bc
hanbinleejoy <lhb1025@gmail.com>
2020-05-29 15:17:22 +0900

    200529 [All] yml 설정, CI 작업

--- cagong-admin-api/src/main/java/com/cagong/caferanking/CaferankingAdminApplication.java
+++ cagong-admin-api/src/main/java/com/cagong/caferanking/CaferankingAdminApplication.java
@@ -1,13 +1,19 @@
 package com.cagong.caferanking;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CaferankingAdminApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:dev.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(CaferankingAdminApplication.class, args);
+		new SpringApplicationBuilder(CaferankingAdminApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
--- cagong-user-api/src/main/java/com/cagong/caferanking/CaferankingUserApplication.java
+++ cagong-user-api/src/main/java/com/cagong/caferanking/CaferankingUserApplication.java
@@ -1,6 +1,5 @@
 package com.cagong.caferanking;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
